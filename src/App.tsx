5254beda6276b91ad443aa75665677ac14b837e7
kirillpotapovnow <kirillpotapovnow@gmail.com>
2024-08-12 13:32:33 +0000

    feat: create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css';
 import { MantineProvider } from '@mantine/core';
-import DateComponent from './components/Date';
 import '@mantine/core/styles.css';
+import NotFoundTitle from './components/404/NotFoundTitle.tsx';
+import Navbar from './components/Navbar.tsx';
 
 function App() {
   return (
-    <>
-      <MantineProvider defaultColorScheme="dark">
-        <DateComponent />
-      </MantineProvider>
-    </>
+    <MantineProvider defaultColorScheme="dark">
+      <NotFoundTitle />
+      <Navbar />
+    </MantineProvider>
   );
 }
 
