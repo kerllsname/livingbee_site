2e03456b9e4c5ef3b003de1dbb603bd1d54445ff
kirillpotapovnow <kirillpotapovnow@gmail.com>
2024-08-28 12:34:32 +0000

    feat: create modal

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,28 +1,80 @@
-import { Card, Container, Center, Group, Text, Title } from '@mantine/core';
+import {
+  Button,
+  Card,
+  Center,
+  Container,
+  Group,
+  Modal,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import DateComponent from './Date';
 // import classes from './Homepage.module.css';
 
 function HomepageComponent() {
+  const [modalOpened, { open: openModal, close: closeModal }] =
+    useDisclosure(false);
   return (
-    <Container fluid>
-      <Center>
-        <Title order={2}>
-          <DateComponent />
-        </Title>
-      </Center>
-      <Container>
-        <Title order={3}>Задачи на сегодня</Title>
-        <Card withBorder>
-          <Group>
-            <Text>Почистить улей</Text>
+    <>
+      <Modal opened={modalOpened} onClose={closeModal} title="Новая задача">
+        <TextInput
+          mt="xs"
+          // ref={taskTitle}
+          placeholder="Task Title"
+          required
+          label="Title"
+        />
+        <TextInput
+          // ref={taskSummary}
+          mt="md"
+          placeholder="Task Summary"
+          label="Summary"
+        />
+        <Group mt="md" justify="space-between">
+          <Button
+            onClick={() => {
+              closeModal();
+            }}
+            variant="subtle"
+          >
+            Закрыть
+          </Button>
+          <Button
+            onClick={() => {
+              // createTask();
+              closeModal();
+            }}
+          >
+            Create Task
+          </Button>
+        </Group>
+      </Modal>
+      <Container fluid m="xl">
+        <Center>
+          <Title order={2}>
+            <DateComponent />
+          </Title>
+        </Center>
+        <Container>
+          <Group justify="space-between" mt="xl" mb="md">
+            <Title order={3}>Задачи на сегодня</Title>
+            <Button onClick={openModal}>Новая задача</Button>
           </Group>
-          <Text size="md" mt="sm">
-            No summary was provided for this task
-          </Text>
-        </Card>
+
+          <Card withBorder>
+            <Group>
+              <Text>Почистить улей</Text>
+            </Group>
+            <Text size="md" mt="sm">
+              No summary was provided for this task
+            </Text>
+          </Card>
+        </Container>
       </Container>
-    </Container>
+    </>
   );
 }
 
